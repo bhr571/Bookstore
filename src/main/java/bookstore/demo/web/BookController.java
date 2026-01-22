7b63baea148307dd652abe6dca0e44341cb2c735
bhr571 <bhr571@myy.haaga-helia.fi>
2026-01-22 16:35:30 +0200

    Alku

---
+++ src/main/java/bookstore/demo/web/BookController.java
@@ -0,0 +1,11 @@
+package bookstore.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class BookController {
+@GetMapping("/index")
+}
